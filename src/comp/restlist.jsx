a1558c5a8835687614b1080470821e5227e0df1d
Siva M <link2siva.m@gmail.com>
2026-03-27 00:22:32 +0530

    Update restlist.jsx

--- src/comp/restlist.jsx
+++ src/comp/restlist.jsx
@@ -7,19 +7,27 @@ function Restlist({ value }) {
     let imgi = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
     let [sl, ssl] = useState(false)
     let dispatch = useDispatch();
+
     function hc() {
         ssl(!sl)
     }
+
     let handleSubmit = (i) => {
         dispatch(addItem(i))
     }
+
     return (
         <div className="rounded-xl bg-white shadow-md overflow-hidden mb-6">
 
+            {/* HEADER */}
             <div
-                className="text-lg font-semibold py-3 px-5 bg-gray-700 text-white cursor-pointer hover:bg-teal-600 transition flex justify-between"
-                onClick={hc}>
-                <div>{value.card.card.title}</div> <div>⮟</div>
+                className="text-base sm:text-lg font-semibold py-3 px-4 sm:px-5 
+                bg-gray-700 text-white cursor-pointer hover:bg-teal-600 
+                transition flex justify-between items-center"
+                onClick={hc}
+            >
+                <div>{value.card.card.title}</div>
+                <div>⮟</div>
             </div>
 
             <div>
@@ -27,36 +35,43 @@ function Restlist({ value }) {
                     value.card.card.itemCards != undefined
                         ? sl && value.card.card.itemCards.map((x, i) =>
 
-                            <div className="flex p-5 gap-6 justify-between border-b border-gray-200 hover:bg-gray-50 transition">
+                            <div className="flex flex-row items-center 
+                            p-3 sm:p-5 gap-3 sm:gap-6 justify-between 
+                            border-b border-gray-200 hover:bg-gray-50 transition">
 
-                                <div className="flex flex-col justify-center gap-2 w-[70%]">
+                                {/* LEFT TEXT */}
+                                <div className="flex flex-col justify-center gap-1 sm:gap-2 w-[65%]">
 
-                                    <div className="font-semibold text-gray-800">
+                                    <div className="font-semibold text-gray-800 text-sm sm:text-base">
                                         {x.card.info.name}
                                     </div>
 
-                                    <div className="text-sm">
+                                    <div className="text-xs sm:text-sm text-gray-600 line-clamp-2">
                                         {x.card.info.description != undefined
                                             ? x.card.info.description
                                             : "Description not available"}
                                     </div>
 
-                                    <div className="text-green-600 font-semibold">
+                                    <div className="text-green-600 font-semibold text-sm sm:text-base">
                                         ₹ {Math.floor(x.card.info.price / 100)}
                                     </div>
 
                                 </div>
 
+                                {/* RIGHT IMAGE */}
                                 <div className="relative flex flex-col items-center">
 
                                     <img
-                                        className="h-25 w-30 object-cover rounded-lg"
+                                        className="h-17.5 w-22.5 sm:h-25 sm:w-30 object-cover rounded-lg"
                                         src={x.card.info.imageId != undefined ? imgi + x.card.info.imageId : no}
                                         alt="no image"
                                     />
 
                                     <button
-                                        className="absolute -bottom-2 bg-gray-200 text-green-600 font-bold px-3 py-1 rounded-md shadow-md border hover:bg-green-50 cursor-pointer"
+                                        className="absolute -bottom-2 text-xs sm:text-sm 
+                                        bg-gray-200 text-green-600 font-bold 
+                                        px-2 sm:px-3 py-1 rounded-md shadow-md border 
+                                        hover:bg-green-50 cursor-pointer"
                                         onClick={() => handleSubmit(x)}
                                     >
                                         ADD +
@@ -73,4 +88,5 @@ function Restlist({ value }) {
         </div>
     )
 }
+
 export default Restlist;
