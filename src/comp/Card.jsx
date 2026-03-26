4b4afae4dbe434ea7c0406cc10272078d3943b4c
Siva M <link2siva.m@gmail.com>
2026-03-27 00:14:54 +0530

    Update Card.jsx

--- src/comp/Card.jsx
+++ src/comp/Card.jsx
@@ -3,15 +3,40 @@ import { motion } from "motion/react"
 
 function Card(i) {
     let img = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
-    return (<Link to={i.val.info.id} className="card w-[20vw] p-4 rounded-2xl hover:shadow-2xl transition-all duration-300 hover:-translate-y-2 cursor-pointer bg-gray-300 ">
-        <motion.div
-            whileHover={{ scale: 1.10 }}
-            whileTap={{ scale: 1 }}
-            transition={{ duration: 0.4 }}
+
+    return (
+        <Link 
+            to={i.val.info.id} 
+            className="card 
+            w-full sm:w-[45%] md:w-[30%] lg:w-[22%] 
+            p-3 sm:p-4 
+            rounded-2xl 
+            hover:shadow-2xl 
+            transition-all duration-300 
+            hover:-translate-y-2 
+            cursor-pointer 
+            bg-gray-300"
         >
-            <img className="io w-full h-[35vh] rounded-2xl object-cover hover:scale-100 transition duration-300" src={img + i.val.info.cloudinaryImageId} />
-            <div>{i.val.info.name}</div>
-            <div>{i.val.info.areaName}</div></motion.div>
-    </Link>)
+            <motion.div
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 1 }}
+                transition={{ duration: 0.3 }}
+            >
+                <img 
+                    className="w-full h-40 sm:h-50 md:h-60 rounded-2xl object-cover" 
+                    src={img + i.val.info.cloudinaryImageId} 
+                />
+
+                <div className="mt-2 font-semibold text-sm sm:text-base">
+                    {i.val.info.name}
+                </div>
+
+                <div className="text-gray-600 text-xs sm:text-sm">
+                    {i.val.info.areaName}
+                </div>
+            </motion.div>
+        </Link>
+    )
 }
+
 export default Card;
